ef605357c039be9c91d2d3c7e1552dd0c684f6cf
wuxiaojian-lingoace <xiaojian.wu@pplingo.com>
2021-10-26 17:31:41 +0800

    first commit

---
+++ src/pages/crop.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Upload } from 'antd';
+import ImgCrop from 'antd-img-crop';
+import 'antd/dist/antd.css';
+import { saveAs } from 'file-saver';
+
+
+const Demo = () => {
+  const [fileList, setFileList] = useState([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+  ]);
+
+  const onChange = ({ fileList: newFileList }) => {
+
+    saveAs(newFileList[1].originFileObj, newFileList[1].name);
+
+  };
+
+  const onPreview = async file => {
+    let src = file.url;
+    if (!src) {
+      src = await new Promise(resolve => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj);
+        reader.onload = () => resolve(reader.result);
+      });
+    }
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
+    imgWindow.document.write(image.outerHTML);
+  };
+
+  return (
+    <ImgCrop 
+    aspect={3/4}
+    minZoom={0.5}
+    rotate grid={true}>
+      <Upload
+        listType="picture-card"
+        fileList={fileList}
+        onChange={onChange}
+        onPreview={onPreview}
+      >
+        {fileList.length < 5 && '+ Upload'}
+      </Upload>
+    </ImgCrop>
+  );
+};
+
+export default Demo;
